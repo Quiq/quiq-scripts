fa0fa4241e49c7732b1c7558fa73053f6fce7ca5
Brian Landblom <brian@quiq.com>
2021-12-16 14:11:15 -0700

    SER-19567: ESLint - Implement for UI projects (remaining repositories)
    1) Updated the ESLint config with a base set of rules to run (the previous version was a test version).
    
    2) Bumped to 1.9.26.

--- config/eslint.js
+++ config/eslint.js
@@ -1,57 +1,12 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', '@quiq'],
+  plugins: ['@typescript-eslint', 'react', '@quiq'],
   ignorePatterns: ['dist/*', 'temp_docker/*', '.*.js', 'babel.config.js'],
   rules: {
-    '@quiq/prevent-cross-app-imports': [
-      'error',
-      {
-        zones: [
-          // Core UI
-          {
-            target: 'core-ui/',
-            from: 'core-agent-ui/',
-            message: 'Cannot import code from core-agent-ui into core-ui',
-          },
-          {
-            target: 'core-ui/',
-            from: 'admin-ui/',
-            message: 'Cannot import code from admin-ui into core-ui',
-          },
-          {
-            target: 'core-ui/',
-            from: 'message-ui/',
-            message: 'Cannot import code from message-ui into core-ui',
-          },
-          {
-            target: 'core-ui/',
-            from: 'outbound-ui/',
-            message: 'Cannot import code from outbound-ui into core-ui',
-          },
-          {
-            target: 'core-ui/',
-            from: 'chat-ui/',
-            message: 'Cannot import code from chat-ui into core-ui',
-          },
-        ],
-      },
-    ],
-
-    '@quiq/prevent-javascript-snippets': [
-      'error',
-      {
-        snippets: [
-          {
-            code: 'var brianTest = true;',
-            directories: ['./'],
-            message: "Cannot use 'var brianTest = true;' within any code.",
-          },
-        ],
-      },
-    ],
-
-    'no-return-assign': 2,
+    // Errors
+    'no-return-assign': ['error'],
+    'react/jsx-no-duplicate-props': ['error'],
     'no-restricted-syntax': [
       'error',
       {
@@ -64,11 +19,15 @@ module.exports = {
         message:
           '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
       },
-      {
-        selector: 'CallExpression[callee.name="encodeURIComponent"]',
-        message:
-          'Params for API calls are URI encoded in ApiMiddleware. Calling this twice will mess up the URI encoding.',
-      },
     ],
+
+    // Warnings
+    'prefer-rest-params': ['warn'],
+    'react/jsx-uses-react': ['warn'],
+    'react/jsx-uses-vars': ['warn'],
+    'react/no-did-update-set-state': ['warn'],
+    'react/no-did-mount-set-state': ['warn'],
+    'react/no-string-refs': ['warn'],
+    'react/no-array-index-key': ['warn'],
   },
 };
