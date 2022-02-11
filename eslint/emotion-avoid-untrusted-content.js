e40a6e812a62d989437c49c13cda0bfa42ded896
Joe DeBruycker <joe.debruycker@goquiq.com>
2022-02-10 19:00:41 -0700

    SER-19820 Add ESLint rule for dynamic content properties

---
+++ eslint/emotion-avoid-untrusted-content.js
@@ -0,0 +1,33 @@
+"use strict";
+
+ module.exports = {
+   meta: {
+     type: 'problem', // `problem`, `suggestion`, or `layout`
+     docs: {
+       description: "Prevents setting the CSS content property dynamically inside styled component string",
+     },
+   },
+
+   create: function emotionAvoidUntrustedContent(context) {
+    const searchString = "content:";
+    const warning = `Avoid setting CSS property "content" to an untrusted dynamic value. If this is a trusted value you can ignore this error.`
+
+    return {
+      TemplateLiteral(node) {
+        const {expressions, quasis} = node;
+        const q = quasis.find(q => q.value.raw.includes(searchString));
+        if (!q) return;
+        const lines = q.value.raw.split('\n');
+        const i = lines.findIndex(l => l.includes(searchString));
+        if (i < 0) return;
+        const lineNo = q.loc.start.line + i;
+        const e = expressions.find(exp => exp.loc.start.line === lineNo);
+        if (e) {
+          context.report({
+            message: warning,
+            node: e
+          })
+        }
+      }
+    };
+ };
--- eslint/main.js
+++ eslint/main.js
@@ -2,5 +2,6 @@ module.exports = {
   rules: {
     'prevent-cross-app-imports': require('./prevent-cross-app-imports'),
     'prevent-javascript-snippets': require('./prevent-javascript-snippets'),
+    'emotion-avoid-untrusted-content': require('./emotion-avoid-untrusted-content');
   },
 };
