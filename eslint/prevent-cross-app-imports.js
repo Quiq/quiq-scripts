9332238625b421bdb46b8f4b6890a7aecbca0fb0
Brian Landblom <brian@quiq.com>
2022-06-03 13:53:33 -0600

    SER-21133: Chat 2.0 - Added ESLint rules for QuiqPlugins
    1) Implemented 'allowTypeImports' which will allow an excluded import if it is an 'import type'.

--- eslint/prevent-cross-app-imports.js
+++ eslint/prevent-cross-app-imports.js
@@ -3,13 +3,6 @@ const moduleVisitor = require('eslint-module-utils/moduleVisitor').default;
 
 const containsPath = (filepath, target) => {
   const relative = path.relative(target, filepath);
-
-  console.log('============================== containsPath() ==============================');
-  console.log('\t- filepath', filepath);
-  console.log('\t- target', target);
-  console.log('\t- path', path);
-  console.log('\t- relative', relative);
-
   return relative === '' || !relative.startsWith('..');
 };
 
@@ -58,6 +51,7 @@ module.exports = {
     const currentFilename = context.getPhysicalFilename
       ? context.getPhysicalFilename()
       : context.getFilename();
+    const sourceCode = context.getSourceCode();
 
     const currentRestrictedZones = restrictedPaths.filter((zone) => {
       const targetPath = path.resolve(basePath, zone.target);
@@ -65,15 +59,11 @@ module.exports = {
     });
 
     function checkForRestrictedImportPath(importPath, node) {
+      const importCode = sourceCode.getText(node.parent);
+
       // (1) Remove '../' and './' from path
       // (2) Remove preceding '/'
       let modifiedImportPath = importPath.replace(/\.\.\//g, '').replace(/\.\//g, '');
-
-      console.log(
-        '============================== checkForRestrictedImportPath() ==============================',
-      );
-      console.log('\t- importPath', importPath);
-      console.log('\t- modifiedImportPath', modifiedImportPath);
 
       if (modifiedImportPath.indexOf('/') === 0) {
         modifiedImportPath = modifiedImportPath.substring(1);
@@ -81,15 +71,13 @@ module.exports = {
 
       // Look at each currently restricted zone and see if it matches the current import path.
       currentRestrictedZones.forEach((zone, index) => {
-        const isExcludedImport = modifiedImportPath.indexOf(zone.from) === 0;
+        let isExcludedImport = modifiedImportPath.indexOf(zone.from) === 0;
 
-        console.log(
-          '============================== currentRestrictedZones.forEach() ==============================',
-        );
-        console.log('\t- index', index);
-        console.log('\t- isExcludedImport', isExcludedImport);
-        console.log('\t- zone', zone);
-        console.log('\t- zone.allowTypeImports', zone.allowTypeImports);
+        // If the import has been excluded check to see if 'type' imports are allowed and
+        // flip the excluded flag to false if it is a type import.
+        if (isExcludedImport && zone.allowTypeImports === true) {
+          isExcludedImport = importCode.toLowerCase().indexOf('import type') !== 0;
+        }
 
         if (isExcludedImport) {
           const message = `${zone.message} ('{{importPath}}').`;
