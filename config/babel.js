6f0970ae54472e0cbf3513733e7b60fa3f665858
Chris Delaney <chris.delaney@quiq.com>
2022-07-25 09:59:05 -0600

    SER-19426: update babel file

--- config/babel.js
+++ config/babel.js
@@ -7,6 +7,8 @@ module.exports = api => {
       [
         "@babel/preset-env",
         {
+          useBuiltIns: "usage",
+          corejs: 3,
           loose: true,
           modules: false,
           targets: {
