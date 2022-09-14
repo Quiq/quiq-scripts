456a39dc30be56e096bf4f45f0351ed8dea8b1c7
Jared Poetter <jared.poetter@quiq.com>
2022-09-14 15:47:41 -0500

    SER-19426 remove deprecated npm repos
    removing @babel/polyfill

--- config/babel.js
+++ config/babel.js
@@ -1,109 +1,111 @@
-module.exports = api => {
+module.exports = (api) => {
   api.cache(true);
 
   return {
     sourceMaps: false,
     presets: [
       [
-        "@babel/preset-env",
+        '@babel/preset-env',
         {
           loose: true,
           modules: false,
           targets: {
-            browsers: ["Chrome >= 50", "Edge >= 12", "FF >= 45", "Safari >= 9"]
-          }
-        }
+            browsers: ['Chrome >= 50', 'Edge >= 12', 'FF >= 45', 'Safari >= 9'],
+          },
+          corejs: '3.25',
+          useBuiltIns: 'usage',
+        },
       ],
-      "@babel/preset-react",
-      "@babel/preset-flow",
-      "@babel/preset-typescript"
+      '@babel/preset-react',
+      '@babel/preset-flow',
+      '@babel/preset-typescript',
     ],
     plugins: [
-      "react-hot-loader/babel",
-      ["emotion", {autoLabel: true}],
-      "@babel/plugin-syntax-dynamic-import",
-      "@babel/plugin-syntax-import-meta",
-      "@babel/plugin-proposal-json-strings",
+      'react-hot-loader/babel',
+      ['emotion', {autoLabel: true}],
+      '@babel/plugin-syntax-dynamic-import',
+      '@babel/plugin-syntax-import-meta',
+      '@babel/plugin-proposal-json-strings',
       [
-        "@babel/plugin-proposal-decorators",
+        '@babel/plugin-proposal-decorators',
         {
-          legacy: true
-        }
+          legacy: true,
+        },
       ],
-      ["@babel/plugin-proposal-class-properties", {loose: true}],
-      "@babel/plugin-proposal-function-sent",
-      "@babel/plugin-proposal-export-namespace-from",
-      "@babel/plugin-proposal-numeric-separator",
-      "@babel/plugin-proposal-throw-expressions",
-      "@babel/plugin-proposal-export-default-from",
-      "@babel/plugin-proposal-logical-assignment-operators",
-      "@babel/plugin-proposal-optional-chaining",
+      ['@babel/plugin-proposal-class-properties', {loose: true}],
+      '@babel/plugin-proposal-function-sent',
+      '@babel/plugin-proposal-export-namespace-from',
+      '@babel/plugin-proposal-numeric-separator',
+      '@babel/plugin-proposal-throw-expressions',
+      '@babel/plugin-proposal-export-default-from',
+      '@babel/plugin-proposal-logical-assignment-operators',
+      '@babel/plugin-proposal-optional-chaining',
       [
-        "@babel/plugin-proposal-pipeline-operator",
+        '@babel/plugin-proposal-pipeline-operator',
         {
-          proposal: "minimal"
-        }
+          proposal: 'minimal',
+        },
       ],
-      "@babel/plugin-proposal-nullish-coalescing-operator",
-      "@babel/plugin-proposal-do-expressions",
-      "@babel/plugin-proposal-function-bind"
+      '@babel/plugin-proposal-nullish-coalescing-operator',
+      '@babel/plugin-proposal-do-expressions',
+      '@babel/plugin-proposal-function-bind',
     ],
     env: {
       test: {
-        presets: ["@babel/preset-env", "@babel/preset-react", "@babel/preset-typescript"],
+        presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
         plugins: [
-          ["emotion", {autoLabel: true}],
-          "@babel/plugin-syntax-dynamic-import",
-          "@babel/plugin-syntax-import-meta",
-          "@babel/plugin-proposal-json-strings",
+          ['emotion', {autoLabel: true}],
+          '@babel/plugin-syntax-dynamic-import',
+          '@babel/plugin-syntax-import-meta',
+          '@babel/plugin-proposal-json-strings',
           [
-            "@babel/plugin-proposal-decorators",
+            '@babel/plugin-proposal-decorators',
             {
-              legacy: true
-            }
+              legacy: true,
+            },
           ],
-          ["@babel/plugin-proposal-class-properties", {loose: true}],
-          "@babel/plugin-proposal-function-sent",
-          "@babel/plugin-proposal-export-namespace-from",
-          "@babel/plugin-proposal-numeric-separator",
-          "@babel/plugin-proposal-throw-expressions",
-          "@babel/plugin-proposal-export-default-from",
-          "@babel/plugin-proposal-logical-assignment-operators",
-          "@babel/plugin-proposal-optional-chaining",
+          ['@babel/plugin-proposal-class-properties', {loose: true}],
+          '@babel/plugin-proposal-function-sent',
+          '@babel/plugin-proposal-export-namespace-from',
+          '@babel/plugin-proposal-numeric-separator',
+          '@babel/plugin-proposal-throw-expressions',
+          '@babel/plugin-proposal-export-default-from',
+          '@babel/plugin-proposal-logical-assignment-operators',
+          '@babel/plugin-proposal-optional-chaining',
           [
-            "@babel/plugin-proposal-pipeline-operator",
+            '@babel/plugin-proposal-pipeline-operator',
             {
-              proposal: "minimal"
-            }
+              proposal: 'minimal',
+            },
           ],
-          "@babel/plugin-proposal-nullish-coalescing-operator",
-          "@babel/plugin-proposal-do-expressions",
-          "@babel/plugin-proposal-function-bind"
-        ]
+          '@babel/plugin-proposal-nullish-coalescing-operator',
+          '@babel/plugin-proposal-do-expressions',
+          '@babel/plugin-proposal-function-bind',
+        ],
       },
       development: {
         plugins: [
           [
-            "emotion",
+            'emotion',
             {
               sourceMap: true,
-              autoLabel: true
-            }
+              autoLabel: true,
+            },
           ],
-          "react-hot-loader/babel"
-        ]
+          'react-hot-loader/babel',
+        ],
       },
       production: {
         plugins: [
           [
-            "emotion",
+            'emotion',
             {
               sourceMap: false,
-              autoLabel: true
-            }
-          ]
-        ]
-      }
-    }
+              autoLabel: true,
+            },
+          ],
+        ],
+      },
+    },
   };
 };
